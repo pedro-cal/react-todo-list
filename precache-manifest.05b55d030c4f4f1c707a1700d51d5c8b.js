396509b56a735762a048aba9c93317528e1e007b
pedro-cal <pedroluna2@hotmail.com>
2020-08-02 22:50:29 -0300

    Updates

---
+++ precache-manifest.05b55d030c4f4f1c707a1700d51d5c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845f72db6c4466443da833af3859a69a",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "a0d4040cea72f2165246",
+    "url": "/react-todo-list/static/css/main.e8a7facb.chunk.css"
+  },
+  {
+    "revision": "16b5fce6d382ddfff113",
+    "url": "/react-todo-list/static/js/2.824e7ef6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.824e7ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d4040cea72f2165246",
+    "url": "/react-todo-list/static/js/main.7ea7b812.chunk.js"
+  },
+  {
+    "revision": "80682d5f29ce18e87584",
+    "url": "/react-todo-list/static/js/runtime-main.28be1446.js"
+  }
+]);
